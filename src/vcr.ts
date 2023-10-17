1937a3aafce92c4b416215bd363931d1cb92998a
Alex H <epignosisx@gmail.com>
2023-10-17 09:21:57 -0400

    feat: allow to change recording mode via env var

--- src/vcr.ts
+++ src/vcr.ts
@@ -3,6 +3,12 @@ import { HttpRequestMasker, ICassetteStorage, IRequestMatcher, RecordMode } from
 import { DefaultRequestMatcher } from './default-request-matcher';
 import { Cassette } from './cassette';
 
+const ENV_TO_RECORD_MODE: Record<string, RecordMode> = {
+  [RecordMode.none]: RecordMode.none,
+  [RecordMode.once]: RecordMode.once,
+  [RecordMode.all]: RecordMode.all,
+} as const;
+
 export class VCR {
 
   public matcher: IRequestMatcher = new DefaultRequestMatcher();
@@ -11,7 +17,8 @@ export class VCR {
   constructor (private readonly storage: ICassetteStorage) {}
 
   public async useCassette(name: string, action: () => Promise<void>) {
-    var cassette = new Cassette(this.storage, this.matcher, name, RecordMode.once, this.requestMasker);
+    const mode = ENV_TO_RECORD_MODE[process.env.VCR_MODE ?? RecordMode.once] ?? RecordMode.once
+    var cassette = new Cassette(this.storage, this.matcher, name, mode, this.requestMasker);
     await cassette.mount();
     try {
       await action();
